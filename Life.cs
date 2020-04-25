a001571f22c58e16249fba7a766cf570fbaefc60
areyesram <aryeg1hu>
2020-04-25 15:58:32 -0500

    separate

---
+++ Life.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace areyesram
+{
+    internal class Life
+    {
+        internal static int Size
+        {
+            get { return _size; }
+            set
+            {
+                _size = value;
+                Cells = new bool[1 + Size + 1, 1 + Size + 1];
+            }
+        }
+
+        internal static bool[,] Cells { get; private set; }
+
+        private static Random _random = new Random();
+        private static int _size;
+
+        internal static void NewGeneration()
+        {
+            var newCells = new bool[1 + Size + 1, 1 + Size + 1];
+            for (var i = 1; i <= Size; i++)
+            {
+                for (var j = 1; j <= Size; j++)
+                {
+                    var neighbors =
+                        (Cells[i - 1, j - 1] ? 1 : 0) +
+                        (Cells[i, j - 1] ? 1 : 0) +
+                        (Cells[i + 1, j - 1] ? 1 : 0) +
+                        (Cells[i - 1, j] ? 1 : 0) +
+                        (Cells[i + 1, j] ? 1 : 0) +
+                        (Cells[i - 1, j + 1] ? 1 : 0) +
+                        (Cells[i, j + 1] ? 1 : 0) +
+                        (Cells[i + 1, j + 1] ? 1 : 0);
+                    if (Cells[i, j])
+                    {
+                        // a cell with 2 or 3 neightbors lives on; else, it dies.
+                        // (less than 2, it dies of loneliness; more than 3, it starves.)
+                        newCells[i, j] = neighbors == 2 || neighbors == 3;
+                    }
+                    else
+                    {
+                        // in an empty space with exactly 3 neighbors, a new cell is born
+                        newCells[i, j] = neighbors == 3;
+                    }
+                }
+            }
+
+            for (var i = 1; i <= Size; i++)
+                for (var j = 1; j <= Size; j++)
+                    Cells[i, j] = newCells[i, j];
+        }
+
+        internal static void Randomize()
+        {
+            for (var i = 1; i <= Size; i++)
+                for (var j = 1; j <= Size; j++)
+                    Cells[i, j] = _random.Next() < int.MaxValue / 2;
+        }
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -7,18 +7,16 @@ namespace areyesram
     public partial class MainForm : Form
     {
         private const int DishSize = 300;
-        private static readonly bool[,] Cells = new bool[1 + DishSize + 1, 1 + DishSize + 1];
-        private static readonly bool[,] NewCells = new bool[1 + DishSize + 1, 1 + DishSize + 1];
-        private static Random _random = new Random();
         private static Graphics _graphics;
-
         private readonly Color _back = Color.FromArgb(164, 161, 118);
         private readonly Color _fore = Color.FromArgb(210, 210, 186);
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             _graphics = Graphics.FromHwnd(Handle);
-            Randomize();
+            Life.Size = DishSize;
+            Life.Randomize();
+            DrawCells();
         }
 
         private void btnStart_Click(object sender, EventArgs e)
@@ -33,54 +31,13 @@ namespace areyesram
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            NextGeneration();
-        }
-
-        private void NextGeneration()
-        {
-            for (var i = 1; i <= DishSize; i++)
-            {
-                for (var j = 1; j <= DishSize; j++)
-                {
-                    var neighbors =
-                        (Cells[i - 1, j - 1] ? 1 : 0) +
-                        (Cells[i, j - 1] ? 1 : 0) +
-                        (Cells[i + 1, j - 1] ? 1 : 0) +
-                        (Cells[i - 1, j] ? 1 : 0) +
-                        (Cells[i + 1, j] ? 1 : 0) +
-                        (Cells[i - 1, j + 1] ? 1 : 0) +
-                        (Cells[i, j + 1] ? 1 : 0) +
-                        (Cells[i + 1, j + 1] ? 1 : 0);
-                    if (Cells[i, j])
-                    {
-                        // a cell with 2 or 3 neightbors lives on; else, it dies.
-                        // (less than 2, it dies of loneliness; more than 3, it starves.)
-                        NewCells[i, j] = neighbors == 2 || neighbors == 3;
-                    }
-                    else
-                    {
-                        // in an empty space with exactly 3 neighbors, a new cell is born
-                        NewCells[i, j] = neighbors == 3;
-                    }
-                }
-            }
-
-            for (var i = 1; i <= DishSize; i++)
-                for (var j = 1; j <= DishSize; j++)
-                    Cells[i, j] = NewCells[i, j];
+            Life.NewGeneration();
             DrawCells();
         }
 
         private void btnRandom_Click(object sender, EventArgs e)
         {
-            Randomize();
-        }
-
-        private void Randomize()
-        {
-            for (var i = 1; i <= DishSize; i++)
-                for (var j = 1; j <= DishSize; j++)
-                    Cells[i, j] = _random.Next() < int.MaxValue / 2;
+            Life.Randomize();
             DrawCells();
         }
 
@@ -93,7 +50,7 @@ namespace areyesram
                 for (var y = 0; y < DishSize; y++)
                 {
                     if (InsideCircle(x, y, DishSize / 2))
-                        Bmp.SetPixel(x, y, Cells[x + 1, y + 1] ? _fore : _back);
+                        Bmp.SetPixel(x, y, Life.Cells[x + 1, y + 1] ? _fore : _back);
                 }
             }
             _graphics.DrawImage(Bmp, 8, 40);
