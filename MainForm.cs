00c2b9b306f01e5c00c08a6add248b369a1e43b1
areyesram <aryeg1hu>
2020-05-02 20:18:28 -0500

    fixes

--- MainForm.cs
+++ MainForm.cs
@@ -7,9 +7,9 @@ namespace areyesram
     public partial class MainForm : Form
     {
         private const int DishSize = 300;
-        private static readonly bool[,] Cells = new bool[1 + DishSize + 1, 1 + DishSize + 1];
-        private static readonly bool[,] NewCells = new bool[1 + DishSize + 1, 1 + DishSize + 1];
-        private static Random _random = new Random();
+        private static readonly bool[,] _cells = new bool[1 + DishSize + 1, 1 + DishSize + 1];
+        private static readonly bool[,] _newCells = new bool[1 + DishSize + 1, 1 + DishSize + 1];
+        private static readonly Random _random = new Random();
         private static Graphics _graphics;
 
         private readonly Color _back = Color.FromArgb(164, 161, 118);
@@ -43,31 +43,31 @@ namespace areyesram
                 for (var j = 1; j <= DishSize; j++)
                 {
                     var neighbors =
-                        (Cells[i - 1, j - 1] ? 1 : 0) +
-                        (Cells[i, j - 1] ? 1 : 0) +
-                        (Cells[i + 1, j - 1] ? 1 : 0) +
-                        (Cells[i - 1, j] ? 1 : 0) +
-                        (Cells[i + 1, j] ? 1 : 0) +
-                        (Cells[i - 1, j + 1] ? 1 : 0) +
-                        (Cells[i, j + 1] ? 1 : 0) +
-                        (Cells[i + 1, j + 1] ? 1 : 0);
-                    if (Cells[i, j])
+                        (_cells[i - 1, j - 1] ? 1 : 0) +
+                        (_cells[i, j - 1] ? 1 : 0) +
+                        (_cells[i + 1, j - 1] ? 1 : 0) +
+                        (_cells[i - 1, j] ? 1 : 0) +
+                        (_cells[i + 1, j] ? 1 : 0) +
+                        (_cells[i - 1, j + 1] ? 1 : 0) +
+                        (_cells[i, j + 1] ? 1 : 0) +
+                        (_cells[i + 1, j + 1] ? 1 : 0);
+                    if (_cells[i, j])
                     {
                         // a cell with 2 or 3 neightbors lives on; else, it dies.
                         // (less than 2, it dies of loneliness; more than 3, it starves.)
-                        NewCells[i, j] = neighbors == 2 || neighbors == 3;
+                        _newCells[i, j] = neighbors == 2 || neighbors == 3;
                     }
                     else
                     {
                         // in an empty space with exactly 3 neighbors, a new cell is born
-                        NewCells[i, j] = neighbors == 3;
+                        _newCells[i, j] = neighbors == 3;
                     }
                 }
             }
 
             for (var i = 1; i <= DishSize; i++)
                 for (var j = 1; j <= DishSize; j++)
-                    Cells[i, j] = NewCells[i, j];
+                    _cells[i, j] = _newCells[i, j];
             DrawCells();
         }
 
@@ -80,11 +80,11 @@ namespace areyesram
         {
             for (var i = 1; i <= DishSize; i++)
                 for (var j = 1; j <= DishSize; j++)
-                    Cells[i, j] = _random.Next() < int.MaxValue / 2;
+                    _cells[i, j] = _random.Next() < int.MaxValue / 2;
             DrawCells();
         }
 
-        private static readonly Bitmap Bmp = new Bitmap(DishSize, DishSize);
+        private static readonly Bitmap _bmp = new Bitmap(DishSize, DishSize);
 
         private void DrawCells()
         {
@@ -93,10 +93,10 @@ namespace areyesram
                 for (var y = 0; y < DishSize; y++)
                 {
                     if (InsideCircle(x, y, DishSize / 2))
-                        Bmp.SetPixel(x, y, Cells[x + 1, y + 1] ? _fore : _back);
+                        _bmp.SetPixel(x, y, _cells[x + 1, y + 1] ? _fore : _back);
                 }
             }
-            _graphics.DrawImage(Bmp, 8, 40);
+            _graphics.DrawImage(_bmp, 8, 40);
         }
 
         /// <summary>
@@ -112,7 +112,7 @@ namespace areyesram
             return (x - r) * (x - r) + (y - r) * (y - r) < r * r;
         }
 
-        private void MainForm_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             _graphics.Dispose();
         }
